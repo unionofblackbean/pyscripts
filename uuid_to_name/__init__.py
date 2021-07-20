1f4e9e83144cdbd9ba5f86bcba555a038dd201a3
Leung Yau Ming <leungyauming2005@gmail.com>
2021-07-20 13:40:41 +0800

    create uuid_to_name package

---
+++ uuid_to_name/__init__.py
@@ -0,0 +1,1 @@
+from .uuid_to_name import *
---
+++ uuid_to_name/uuid_to_name.py
@@ -0,0 +1,26 @@
+import json
+import uuid
+import requests
+import typing
+
+
+def uuid_to_current_name(uuid_: uuid.UUID) -> str:
+    history = uuid_to_name_history(uuid_)
+    return history[0] if history else None
+
+
+def uuid_to_name_history(uuid_: uuid.UUID) -> typing.List[str]:
+    if uuid_:
+        res = requests.get(f"https://api.mojang.com/user/profiles/{str(uuid_)}/names")
+        if res.status_code != 200:
+            return None
+
+        names = []
+        res_j = json.loads(res.text)
+        for name_history in res_j:
+            name = name_history.get("name")
+            if name:
+                names.append(name)
+        return names
+    else:
+        return None
